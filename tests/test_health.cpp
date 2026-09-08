966720c5f1c92ec461f9850dea70edee422ec919
Megaraj P <megaraj.student@gmail.com>
2026-09-08 18:37:07 +0530

    Update test_health.cpp

--- tests/test_health.cpp
+++ tests/test_health.cpp
@@ -1,13 +1,23 @@
 #include "autocan/Config.hpp"
 #include "autocan/Health.hpp"
 #include <gtest/gtest.h>
+
 using namespace autocan;
-class ConfiguredHealthTest : public ::testing::Test {
+
+class ConfiguredHealthTest : public ::testing::Test
+{
 protected:
-    ConfiguredHealthTest() : config("..") { HealthPolicy::configure(&config); }
+    ConfiguredHealthTest()
+        : config("..")
+    {
+        HealthPolicy::configure(&config);
+    }
+
     Config config;
 };
-TEST_F(ConfiguredHealthTest, FloorsComeFromConfiguration) {
+
+TEST_F(ConfiguredHealthTest, FloorsComeFromConfiguration)
+{
     EXPECT_EQ(55, HealthPolicy::floor(FaultType::MessageDrop));
     EXPECT_EQ(70, HealthPolicy::floor(FaultType::TransmissionDelay));
     EXPECT_EQ(35, HealthPolicy::floor(FaultType::Timeout));
@@ -15,13 +25,66 @@ TEST_F(ConfiguredHealthTest, FloorsComeFromConfiguration) {
     EXPECT_EQ(30, HealthPolicy::floor(FaultType::TransmissionFailure));
     EXPECT_EQ(15, HealthPolicy::floor(FaultType::EcuOffline));
 }
-TEST_F(ConfiguredHealthTest, StatusThresholdsComeFromConfiguration) {
+
+TEST_F(ConfiguredHealthTest, StatusThresholdsComeFromConfiguration)
+{
     EXPECT_EQ(EcuStatus::Healthy, HealthPolicy::statusFor(90));
     EXPECT_EQ(EcuStatus::Warning, HealthPolicy::statusFor(70));
     EXPECT_EQ(EcuStatus::Critical, HealthPolicy::statusFor(30));
     EXPECT_EQ(EcuStatus::Offline, HealthPolicy::statusFor(29));
 }
-TEST_F(ConfiguredHealthTest, RecoveryIncrementComesFromConfiguration) {
+
+TEST_F(ConfiguredHealthTest, RecoveryIncrementComesFromConfiguration)
+{
     EXPECT_EQ(62, HealthPolicy::recover(55));
     EXPECT_EQ(100, HealthPolicy::recover(99));
+}
+
+TEST_F(ConfiguredHealthTest, MessageDropDegradationValue)
+{
+    EXPECT_EQ(
+        4,
+        HealthPolicy::degradation(FaultType::MessageDrop, 1));
+}
+
+TEST_F(ConfiguredHealthTest, DelayDegradationValue)
+{
+    EXPECT_EQ(
+        2,
+        HealthPolicy::degradation(FaultType::TransmissionDelay, 1));
+}
+
+TEST_F(ConfiguredHealthTest, TimeoutDegradationValue)
+{
+    EXPECT_EQ(
+        6,
+        HealthPolicy::degradation(FaultType::Timeout, 1));
+}
+
+TEST_F(ConfiguredHealthTest, InvalidPayloadDegradationValue)
+{
+    EXPECT_EQ(
+        3,
+        HealthPolicy::degradation(FaultType::InvalidPayload, 1));
+}
+
+TEST_F(ConfiguredHealthTest, TransmissionFailureDegradationValue)
+{
+    EXPECT_EQ(
+        7,
+        HealthPolicy::degradation(FaultType::TransmissionFailure, 1));
+}
+
+TEST_F(ConfiguredHealthTest, EcuOfflineDegradationValue)
+{
+    EXPECT_EQ(
+        9,
+        HealthPolicy::degradation(FaultType::EcuOffline, 1));
+}
+
+TEST_F(ConfiguredHealthTest, HealthRecoveryDoesNotExceed100)
+{
+    EXPECT_EQ(100, HealthPolicy::recover(95));
+    EXPECT_EQ(100, HealthPolicy::recover(99));
+    EXPECT_EQ(100, HealthPolicy::recover(100));
 }
